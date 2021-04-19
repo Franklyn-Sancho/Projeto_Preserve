0d8e4ba90be7754e1973ba070530c9f1a90fe851
Franklyn-Sancho <Franklyn.hs@gmail.com>
2021-04-19 18:06:32 -0300

    Construção template

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './template/Header'
+import Main from './template/Main'
+
+
+
+function App() {
+  return (
+    <div className="App">  
+   		<Header />
+   		<Main />
+    </div>
+  );
+}
+
+export default App;
